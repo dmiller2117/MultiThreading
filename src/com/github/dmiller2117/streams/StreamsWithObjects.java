d7ec206e361c1906a0fa2e0eb5bfa3090c458f36
David Miller <dmiller2117@gmail.com>
2021-08-26 18:30:23 +0100

    Streams with objects

--- src/com/github/dmiller2117/streams/StreamsWithObjects.java
+++ src/com/github/dmiller2117/streams/StreamsWithObjects.java
@@ -24,12 +24,13 @@ public class StreamsWithObjects {
         students.stream().filter(s -> s.isLocal() == true).forEach(s -> System.out.println(s.getName()));
 
         System.out.println("count the local students:");
-        // using a method reference for isLocal
+        // using a method reference for isLocal()
         long count = students.stream().filter(Student::isLocal).count();
         System.out.printf("There are %d local students", count);
         System.out.println();
 
         System.out.println("we can construct a single string of the local students:");
+        // using a lambda for isLocal()
         String names = students.stream().filter(s -> s.isLocal()).map(Student::getName).collect(Collectors.joining(" "));
         System.out.println(names);
 
